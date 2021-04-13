55881634b4b3267015f529c30f30a5b891f68710
soujanya-rai <68293437+soujanya-rai@users.noreply.github.com>
2021-04-13 15:33:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var towerImg, tower;
+var doorImg, door, doorsGroup;
+var climberImg, climber, climbersGroup;
+var ghost, ghostImg;
+var invisibleBlockGroup, invisibleBlock;
+var gameState = "play"
+
+function preload(){
+}
+
+function setup(){
+}
+
+function draw(){
+
+}
+
+function spawnDoors() {
+  //write code here to spawn the doors in the tower
+}
+
